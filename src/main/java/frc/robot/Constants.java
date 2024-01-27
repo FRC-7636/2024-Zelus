d8987202421299fc2e5660e42882dd8f845b17f1
Allen95Wei <49893966+Alllen95Wei@users.noreply.github.com>
2024-01-27 11:00:58 +0800

    fix typo

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,7 +8,7 @@ import swervelib.math.Matter;
 public class Constants {
     public class Chassis {
         public static final double ROBOT_MASS = 23.05;  //kg
-        public static final double LOOP_TIME = 0.13;  //s, 20ms + 110ms sprk max velocity lag
+        public static final double LOOP_TIME = 0.13;  //s, 20ms + 110ms SPARK MAX velocity lag
         public static final Matter CHASSIS = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
     }
 }
