a0c42e21b8858c59977cd224da26855e3a52c0e7
SophiaPiiiiig <bennliao97@gmail.com>
2024-02-02 09:22:10 +0800

    新增subsystem,路徑

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,4 +11,27 @@ public class Constants {
         public static final double LOOP_TIME = 0.13;  //s, 20ms + 110ms SPARK MAX velocity lag
         public static final Matter CHASSIS = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
     }
+
+    public class IntakeConstants{
+        public static final int pipeIntakePort = 0;
+        public static final int angleIntakePort = 0;
+
+        public static final double kP = 0;
+        public static final double kI = 0;
+        public static final double kD = 0;
+    }
+
+    public class drivePIDF{
+        public static final double driveKP = 0.07;
+        public static final double driveKI = 0;
+        public static final double driveKD = 0;
+        public static final double driveIzone = 0;                
+    }
+
+    public class turnPIDF{
+        public static final double turnKP = 1;
+        public static final double turnKI = 0.003;
+        public static final double turnKD = 0.005;
+        public static final double turnIzone = 0;
+    }
 }
