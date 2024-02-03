8f5333b19535a966483b74b734b6d95b2c3c63e5
Allen95Wei <49893966+Alllen95Wei@users.noreply.github.com>
2024-02-03 15:26:17 +0800

    1. 自動push電池電壓 2. 補上自動程式

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.RobotController;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
@@ -23,6 +24,7 @@ public class Robot extends TimedRobot {
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
     m_robotContainer.sendGamePadValueToDashboard();
+    SmartDashboard.putNumber("Battery Voltage", RobotController.getBatteryVoltage());
   }
 
   @Override
@@ -37,6 +39,11 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    if (m_autonomousCommand != null)
+    {
+      m_autonomousCommand.schedule();
+    }
   }
 
   @Override
