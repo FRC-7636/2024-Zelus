b7c3d2049567950c2a137d32e5ca2b1e9256bf85
Allen95Wei <49893966+Alllen95Wei@users.noreply.github.com>
2024-02-04 20:45:39 +0800

    fix typo
    Signed-off-by: Allen95Wei <49893966+Alllen95Wei@users.noreply.github.com>

--- src/main/java/frc/robot/commands/MiddleStart.java
+++ src/main/java/frc/robot/commands/MiddleStart.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 public class MiddleStart extends SequentialCommandGroup{
     public MiddleStart(PhotonVision m_photonVision, Swerve m_swerve){
-        addCommands(Commands.runOnce(()->m_swerve.resetOdometry(m_photonVision.getLatestEstimatedRobotPose()), m_swerve, m_photonVision));
+        addCommands(Commands.runOnce(() -> m_swerve.resetOdometry(m_photonVision.getLatestEstimatedRobotPose()), m_swerve, m_photonVision));
         addCommands(m_swerve.getAutonomousCommand("1", false));
         addCommands(m_swerve.getAutonomousCommand("2", false));
         addCommands(m_swerve.getAutonomousCommand("2 Shoot", false));
