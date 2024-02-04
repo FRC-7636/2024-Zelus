001ed2c8b6f8f771fcb218f5c546a8f1b8ed325d
Allen95Wei <49893966+Alllen95Wei@users.noreply.github.com>
2024-02-04 17:09:54 +0800

    1. fix typo 2. 將所有Constants中的類別設為static
    Signed-off-by: Allen95Wei <49893966+Alllen95Wei@users.noreply.github.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,20 +6,20 @@ import edu.wpi.first.math.util.Units;
 import swervelib.math.Matter;
 
 public class Constants {
-    public class Chassis {
+    public static class Chassis {
         public static final double ROBOT_MASS = 23.05;  //kg
         public static final double LOOP_TIME = 0.13;  //s, 20ms + 110ms SPARK MAX velocity lag
         public static final Matter CHASSIS = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
     }
 
-    public class drivePIDF{
+    public static class drivePIDF {
         public static final double driveKP = 0.07;
         public static final double driveKI = 0.007;
         public static final double driveKD = 0;
-        public static final double driveIzone = 0;                
+        public static final double driveIzone = 0;
     }
 
-    public class turnPIDF{
+    public static class turnPIDF {
         public static final double turnKP = 1;
         public static final double turnKI = 0.003;
         public static final double turnKD = 0.005;
