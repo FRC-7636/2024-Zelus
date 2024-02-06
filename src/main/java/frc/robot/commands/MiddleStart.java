6bb68a0a04fbf91fd4d6ca68a76969ec28da679d
SophiaPiiiiig <bennliao97@gmail.com>
2024-02-06 19:49:04 +0800

    下拉式Auto測試成功

--- src/main/java/frc/robot/commands/MiddleStart.java
+++ src/main/java/frc/robot/commands/MiddleStart.java
@@ -1,14 +1,15 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.subsystems.PhotonVision;
+// import frc.robot.subsystems.PhotonVision;
+import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.Swerve;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 public class MiddleStart extends SequentialCommandGroup{
-    public MiddleStart(PhotonVision m_photonVision, Swerve m_swerve){
-        addCommands(Commands.runOnce(() -> m_swerve.resetOdometry(m_photonVision.getLatestEstimatedRobotPose()), m_swerve, m_photonVision));
+    public MiddleStart(Limelight limelight, Swerve m_swerve){
+        addCommands(Commands.runOnce(() -> m_swerve.resetOdometry(limelight.getPose2d()), m_swerve, limelight));
         addCommands(m_swerve.getAutonomousCommand("1", false));
         addCommands(m_swerve.getAutonomousCommand("2", false));
         addCommands(m_swerve.getAutonomousCommand("2 Shoot", false));
