6d9c0917d5b0291ce3587bf408c73126a380e7a6
Allen95Wei <allen95wei@gmail.com>
2024-02-06 20:05:47 +0800

    解除註解
    Signed-off-by: Allen95Wei <allen95wei@gmail.com>

--- src/main/java/frc/robot/subsystems/PhotonVision.java
+++ src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -1,80 +1,80 @@
-// package frc.robot.subsystems;
+ package frc.robot.subsystems;
 
-// import edu.wpi.first.apriltag.AprilTagFieldLayout;
-// import edu.wpi.first.apriltag.AprilTagFields;
-// import edu.wpi.first.math.geometry.Pose2d;
-// import edu.wpi.first.math.geometry.Pose3d;
-// import edu.wpi.first.math.geometry.Rotation3d;
-// import edu.wpi.first.math.geometry.Transform3d;
-// import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
+ import edu.wpi.first.apriltag.AprilTagFieldLayout;
+ import edu.wpi.first.apriltag.AprilTagFields;
+ import edu.wpi.first.math.geometry.Pose2d;
+ import edu.wpi.first.math.geometry.Pose3d;
+ import edu.wpi.first.math.geometry.Rotation3d;
+ import edu.wpi.first.math.geometry.Transform3d;
+ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-// import java.util.Optional;
+ import java.util.Optional;
 
-// import org.photonvision.PhotonCamera;
-// import org.photonvision.PhotonUtils;
-// import org.photonvision.targeting.PhotonPipelineResult;
-// import org.photonvision.targeting.PhotonTrackedTarget;
+ import org.photonvision.PhotonCamera;
+ import org.photonvision.PhotonUtils;
+ import org.photonvision.targeting.PhotonPipelineResult;
+ import org.photonvision.targeting.PhotonTrackedTarget;
 
-// import frc.robot.Constants.VisionConstants;
+ import frc.robot.Constants.VisionConstants;
 
-// public class PhotonVision extends SubsystemBase {
+ public class PhotonVision extends SubsystemBase {
 
-//   private PhotonCamera photonCamera;
-//   private AprilTagFieldLayout m_layout;
-//   public Field2d m_field2d = new Field2d();
+   private PhotonCamera photonCamera;
+   private AprilTagFieldLayout m_layout;
+   public Field2d m_field2d = new Field2d();
 
-//   // private static final Transform3d camera_to_robot = new Transform3d(-17, 36.5, -70, new Rotation3d(0, 0.125*Math.PI, 0));
-//   /**
-//    * Standard deviations of the vision measurements. Increase these numbers to trust global measurements from vision
-//    * less. This matrix is in the form [x, y, theta]ᵀ, with units in meters and radians.
-//    */
+   // private static final Transform3d camera_to_robot = new Transform3d(-17, 36.5, -70, new Rotation3d(0, 0.125*Math.PI, 0));
+   /**
+    * Standard deviations of the vision measurements. Increase these numbers to trust global measurements from vision
+    * less. This matrix is in the form [x, y, theta]ᵀ, with units in meters and radians.
+    */
 
-//   public PhotonVision() {
-//     photonCamera = new PhotonCamera(VisionConstants.NAME);
-//     try{
-//       m_layout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2024Crescendo.m_resourceFile);
-//     } 
-//     catch(Exception error){
-//       throw new RuntimeException(error);
-//     } 
-//   }
+   public PhotonVision() {
+     photonCamera = new PhotonCamera(VisionConstants.NAME);
+     try{
+       m_layout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2024Crescendo.m_resourceFile);
+     }
+     catch(Exception error){
+       throw new RuntimeException(error);
+     }
+   }
 
-//   public PhotonTrackedTarget getBestTarget() {
-//     PhotonPipelineResult result = photonCamera.getLatestResult();
+   public PhotonTrackedTarget getBestTarget() {
+     PhotonPipelineResult result = photonCamera.getLatestResult();
 
-//     boolean hasTarget = result.hasTargets();
+     boolean hasTarget = result.hasTargets();
 
-//     PhotonTrackedTarget target = null;
+     PhotonTrackedTarget target = null;
 
-//     if (hasTarget) {
-//         target = result.getBestTarget();
-//     }
+     if (hasTarget) {
+         target = result.getBestTarget();
+     }
 
-//     return target;
-//   }
+     return target;
+   }
 
-//   public Pose2d getLatestEstimatedRobotPose() {
-//     PhotonTrackedTarget target = getBestTarget();
+   public Pose2d getLatestEstimatedRobotPose() {
+     PhotonTrackedTarget target = getBestTarget();
 
-//     if (target != null) {
-//       Transform3d cameraToTarget = target.getBestCameraToTarget();
+     if (target != null) {
+       Transform3d cameraToTarget = target.getBestCameraToTarget();
 
-//       Optional<Pose3d> tagPose = m_layout.getTagPose(target.getFiducialId());
+       Optional<Pose3d> tagPose = m_layout.getTagPose(target.getFiducialId());
 
 
-//       if (tagPose.isPresent()) {
-//           Pose3d robotPose = PhotonUtils.estimateFieldToRobotAprilTag(cameraToTarget, tagPose.get(), VisionConstants.CAMERA_TO_ROBOT);
-//           return robotPose.toPose2d();
-//       }
-//     }
-//     return new Pose2d();
-//   }
+       if (tagPose.isPresent()) {
+           Pose3d robotPose = PhotonUtils.estimateFieldToRobotAprilTag(cameraToTarget, tagPose.get(), VisionConstants.CAMERA_TO_ROBOT);
+           return robotPose.toPose2d();
+       }
+     }
+     return new Pose2d();
+   }
 
-//   @Override
-//   public void periodic() {
-//     m_field2d.setRobotPose(getLatestEstimatedRobotPose());
-//     SmartDashboard.putData("m_Field2d", m_field2d);
-//   }
-// }
+   @Override
+   public void periodic() {
+     m_field2d.setRobotPose(getLatestEstimatedRobotPose());
+     SmartDashboard.putData("m_Field2d", m_field2d);
+   }
+ }
