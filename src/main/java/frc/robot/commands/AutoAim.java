a57cb7e38a34faf95a75d9db5847d698cdae116e
Allen95Wei <allen95wei@gmail.com>
2024-02-18 16:00:06 +0800

    移動程式碼至execute
    Signed-off-by: Allen95Wei <allen95wei@gmail.com>

--- src/main/java/frc/robot/commands/AutoAim.java
+++ src/main/java/frc/robot/commands/AutoAim.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -8,6 +9,7 @@ import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import frc.robot.subsystems.PhotonVision;
 import frc.robot.subsystems.Swerve;
@@ -20,7 +22,6 @@ import java.util.ArrayList;
 public class AutoAim extends Command {
 //    private final PhotonVision photonVision;
     private final Swerve swerve;
-    private Trajectory trajToTargetPose;
 
 //    public AutoAim(PhotonVision photonVision, Swerve swerve) {
 //        this.photonVision = photonVision;
@@ -37,18 +38,25 @@ public class AutoAim extends Command {
     }
 
     @Override
-    public void initialize() {
-        Rotation2d targetHeading = new Rotation2d(Units.degreesToRadians(LimelightHelpers.getTX(VisionConstants.NAME)));
-        Pose2d targetPose = swerve.getPose().rotateBy(targetHeading);
-        trajToTargetPose = TrajectoryGenerator.generateTrajectory(
-                swerve.getPose(),
-                new ArrayList<>(),
-                targetPose,
-                new TrajectoryConfig(10, 10));
-    }
-
-    @Override
     public void execute() {
+        double deltaDeg = Units.degreesToRadians(LimelightHelpers.getTX(VisionConstants.NAME));
+        double tagId = LimelightHelpers.getFiducialID(VisionConstants.NAME);
+        SmartDashboard.putNumber("Delta Deg.", deltaDeg);
+        SmartDashboard.putNumber("Current ID", tagId);
+        Trajectory trajToTargetPose;
+        if (tagId == 4d || tagId == 7d) {
+            Rotation2d targetHeading = new Rotation2d(deltaDeg);
+            Pose2d targetPose = swerve.getPose().rotateBy(targetHeading);
+            trajToTargetPose = TrajectoryGenerator.generateTrajectory(
+                    swerve.getPose(),
+                    new ArrayList<>(),
+                    targetPose,
+                    new TrajectoryConfig(10, 10));
+        }
+        else {
+            trajToTargetPose = new Trajectory();
+        }
+
         swerve.postTrajectory(trajToTargetPose);
     }
 
