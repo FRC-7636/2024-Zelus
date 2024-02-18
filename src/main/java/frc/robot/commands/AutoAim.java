78965eb2d277c25c93103b5f660d137a58770ba3
Allen95Wei <allen95wei@gmail.com>
2024-02-18 12:59:06 +0800

    backup
    Signed-off-by: Allen95Wei <allen95wei@gmail.com>

--- src/main/java/frc/robot/commands/AutoAim.java
+++ src/main/java/frc/robot/commands/AutoAim.java
@@ -1,25 +1,55 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+
 import frc.robot.subsystems.PhotonVision;
 import frc.robot.subsystems.Swerve;
+import frc.robot.Constants.VisionConstants;
+import frc.robot.LimelightHelpers;
+
+import java.util.ArrayList;
 
 
 public class AutoAim extends Command {
-    private final PhotonVision photonVision;
+//    private final PhotonVision photonVision;
     private final Swerve swerve;
+    private Trajectory trajToTargetPose;
 
-    public AutoAim(PhotonVision photonVision, Swerve swerve) {
-        this.photonVision = photonVision;
+//    public AutoAim(PhotonVision photonVision, Swerve swerve) {
+//        this.photonVision = photonVision;
+//        this.swerve = swerve;
+//        // each subsystem used by the command must be passed into the
+//        // addRequirements() method (which takes a vararg of Subsystem)
+//        addRequirements(this.photonVision, this.swerve);
+//    }
+
+    public AutoAim(Swerve swerve) {
         this.swerve = swerve;
-        // each subsystem used by the command must be passed into the
-        // addRequirements() method (which takes a vararg of Subsystem)
-        addRequirements(this.photonVision, this.swerve);
+
+        addRequirements(this.swerve);
+    }
+
+    @Override
+    public void initialize() {
+        Rotation2d targetHeading = new Rotation2d(Units.degreesToRadians(LimelightHelpers.getTX(VisionConstants.NAME)));
+        Pose2d targetPose = swerve.getPose().rotateBy(targetHeading);
+        trajToTargetPose = TrajectoryGenerator.generateTrajectory(
+                swerve.getPose(),
+                new ArrayList<>(),
+                targetPose,
+                new TrajectoryConfig(10, 10));
     }
 
     @Override
     public void execute() {
-
+        swerve.postTrajectory(trajToTargetPose);
     }
 
     @Override
