20859a13d8d5da6fdcd88363011ff474c44294f6
Allen95Wei <allen95wei@gmail.com>
2024-02-22 19:28:38 +0800

    新增GetNoteFromFloor指令
    Signed-off-by: Allen95Wei <allen95wei@gmail.com>

---
+++ src/main/java/frc/robot/commands/GetNoteFromFloor.java
@@ -0,0 +1,37 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
+
+
+public class GetNoteFromFloor extends Command {
+    private final Intake intake;
+    private final Shooter shooter;
+
+    public GetNoteFromFloor(Intake intake, Shooter shooter) {
+        this.intake = intake;
+        this.shooter = shooter;
+
+        addRequirements(this.intake, this.shooter);
+    }
+
+    @Override
+    public void execute() {
+        intake.floorAngle();
+        intake.suck();
+        shooter.transport();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        shooter.stopTransport();
+        intake.stop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return shooter.noteDetected();
+    }
+}
