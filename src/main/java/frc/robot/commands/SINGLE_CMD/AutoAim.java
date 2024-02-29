d067f6293a620322a128df19c59c32fda2b424f5
Allen95Wei <allen95wei@gmail.com>
2024-02-29 17:27:15 +0800

    在AutoAim中加入shooter角度校正
    Signed-off-by: Allen95Wei <allen95wei@gmail.com>

--- src/main/java/frc/robot/commands/SINGLE_CMD/AutoAim.java
+++ src/main/java/frc/robot/commands/SINGLE_CMD/AutoAim.java
@@ -1,12 +1,13 @@
 package frc.robot.commands.SINGLE_CMD;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.math.controller.PIDController;
 
+import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Swerve;
 import frc.robot.LimelightHelpers;
 import frc.robot.Constants.AutoAimPID;
@@ -15,6 +16,7 @@ import frc.robot.Constants.AutoAimPID;
 public class AutoAim extends Command {
 //    private final PhotonVision photonVision;
     private final Swerve swerve;
+    private final Shooter shooter;
     private final PIDController yawCtrl = new PIDController(AutoAimPID.P, AutoAimPID.I, AutoAimPID.D);
 
 //    public AutoAim(PhotonVision photonVision, Swerve swerve) {
@@ -25,29 +27,35 @@ public class AutoAim extends Command {
 //        addRequirements(this.photonVision, this.swerve);
 //    }
 
-    public AutoAim(Swerve swerve) {
+    public AutoAim(Swerve swerve, Shooter shooter) {
         this.swerve = swerve;
+        this.shooter = shooter;
 
-        addRequirements(this.swerve);
+        addRequirements(this.swerve, this.shooter);
     }
 
     @Override
     public void execute() {
-        double deltaDeg = LimelightHelpers.getTX("");
-        double deltaRad = Units.degreesToRadians(deltaDeg);
-        SmartDashboard.putNumber("Delta Rad.", deltaDeg);
+        double deltaDegX = LimelightHelpers.getTX("");
+        double deltaRad = Math.toRadians(deltaDegX);
+        SmartDashboard.putNumber("Delta Rad.", deltaDegX);
         Rotation2d targetHeading = swerve.getHeading().plus(new Rotation2d(deltaRad));
         SmartDashboard.putNumber("Heading offset", targetHeading.getDegrees());
         if (!isFinished()) {
-            swerve.drive(new Translation2d(), yawCtrl.calculate(deltaDeg), false);
+            swerve.drive(new Translation2d(), yawCtrl.calculate(deltaDegX), false);
         } else {
             swerve.drive(new Translation2d(), 0, false);
         }
+
+        Pose3d robotToTarget = LimelightHelpers.getBotPose3d_TargetSpace("");
+        double deltaDegY = 90 - Math.toDegrees(Math.atan2(-robotToTarget.getZ(), robotToTarget.getY() + 0.45));
+        System.out.println(deltaDegY);
+        shooter.setPosition(deltaDegY - 16);
     }
 
     @Override
     public boolean isFinished() {
         double deltaDeg = Math.abs(LimelightHelpers.getTX(""));
-        return deltaDeg < 5;
+        return deltaDeg < 1;
     }
 }
