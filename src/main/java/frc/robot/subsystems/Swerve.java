83e4c008deab86c27e67e51919df1af789f54029
Allen95Wei <allen95wei@gmail.com>
2024-03-01 15:54:02 +0800

    更改遙測資料為`LOW`
    Signed-off-by: Allen95Wei <allen95wei@gmail.com>

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -77,7 +77,7 @@ public class Swerve extends SubsystemBase{
     System.out.println("}");
 
     // Configure the Telemetry before creating the SwerveDrive to avoid unnecessary objects being created.
-    SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+    SwerveDriveTelemetry.verbosity = TelemetryVerbosity.LOW;
     try
     {
       // swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed);
