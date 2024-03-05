e765552b7de18b722957dc37b6eab7a8a7e9b6bf
Allen95Wei <allen95wei@gmail.com>
2024-03-05 18:04:50 +0300

    當偵測到NOTE時自動更改顏色
    Signed-off-by: Allen95Wei <allen95wei@gmail.com>

--- src/main/java/frc/robot/subsystems/Candle.java
+++ src/main/java/frc/robot/subsystems/Candle.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.led.CANdle;
 import com.ctre.phoenix.led.StrobeAnimation;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.Constants.OthersConstants;
@@ -10,15 +11,32 @@ import frc.robot.Constants.OthersConstants;
 public class Candle extends SubsystemBase {
     private final CANdle candle = new CANdle(OthersConstants.CANDLE_ID, "cantivore");
 
-    private final StrobeAnimation normalState = new StrobeAnimation(255, 0, 255, 0, 0.5, 8);
+    private final StrobeAnimation normalState = new StrobeAnimation(142, 87, 22);
+    private final StrobeAnimation detectedState = new StrobeAnimation(0, 0, 255);
+
     public Candle() {
         candle.configFactoryDefault();
 
         candle.clearAnimation(0);
+        candle.setLEDs(255, 255, 255);
     }
 
     public void backToNormalState() {
-        candle.animate(normalState);
+        candle.setLEDs(10, 10, 10);
+    }
+
+    public void noteDetectedState() {
+        candle.setLEDs(255, 0, 255);
+    }
+
+    @Override
+    public void periodic() {
+        if (SmartDashboard.getBoolean("NOTE Detected?", false)) {
+            noteDetectedState();
+        }
+        else {
+            backToNormalState();
+        }
     }
 }
 
