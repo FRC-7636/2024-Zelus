89c214b4e3cc58e3762eca76e894d738cb596c70
Allen95Wei <allen95wei@gmail.com>
2024-03-06 17:36:07 +0300

    上升至AMP高度時，shooter會自動降低，且在誤差小於2.5時才啟動吊掛
    Signed-off-by: Allen95Wei <allen95wei@gmail.com>

--- src/main/java/frc/robot/commands/GROUP_CMD/Amp.java
+++ src/main/java/frc/robot/commands/GROUP_CMD/Amp.java
@@ -1,13 +1,38 @@
 package frc.robot.commands.GROUP_CMD;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.robot.Constants.ShooterConstants;
 import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Intake;
 
-public class Amp extends SequentialCommandGroup {
-    public Amp(Climber climber, Intake intake) {
-        addCommands(new InstantCommand(climber::setBalanceLevel, climber));
-        addCommands(new InstantCommand(intake::ampAngle, intake));
+
+public class Amp extends Command {
+    private final Climber climber;
+    private final Shooter shooter;
+    private final Intake intake;
+
+    public Amp(Climber climber, Shooter shooter, Intake intake) {
+        this.climber = climber;
+        this.shooter = shooter;
+        this.intake = intake;
+        // each subsystem used by the command must be passed into the
+        // addRequirements() method (which takes a vararg of Subsystem)
+        addRequirements(this.climber, this.shooter, this.intake);
+    }
+
+    @Override
+    public void execute() {
+        shooter.setPosition(5);
+        if (Math.abs(shooter.currentPosition() - 5) <= 2.5) {
+            climber.setBalanceLevel();
+            intake.ampAngle();
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        climber.stop();
     }
 }
