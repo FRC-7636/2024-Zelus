7727d3f722a668131b22298ed8900c2a1b795138
Allen95Wei <allen95wei@gmail.com>
2024-03-08 00:55:41 +0300

    重構
    Signed-off-by: Allen95Wei <allen95wei@gmail.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -30,122 +30,122 @@ import frc.robot.commands.SWERVE_CMD.NewFieldDrive;
 
 
 public class RobotContainer {
-  // Objects, Instances
-  private final Swerve driveBase = new Swerve(new File(Filesystem.getDeployDirectory(), "swerve/Neo"));
-  private final Shooter shooter = new Shooter();
-  private final Intake intake = new Intake();
-  private final Climber climber = new Climber();
-  private final Candle candle = new Candle();
-  private final XboxController chassisCtrl = new XboxController(0);
-  private final XboxController assistCtrl = new XboxController(1);
-  //  private final PhotonVision photonVision = new PhotonVision();
+    // Objects, Instances
+    private final Swerve driveBase = new Swerve(new File(Filesystem.getDeployDirectory(), "swerve/Neo"));
+    private final Shooter shooter = new Shooter();
+    private final Intake intake = new Intake();
+    private final Climber climber = new Climber();
+    private final Candle candle = new Candle();
+    private final XboxController chassisCtrl = new XboxController(0);
+    private final XboxController assistCtrl = new XboxController(1);
+    //  private final PhotonVision photonVision = new PhotonVision();
 
-  // Auto Commands
-  private final MiddleStart middleStart = new MiddleStart(driveBase, shooter, intake);
-  private final LeftStart leftStart = new LeftStart(driveBase);
-  private final SendableChooser<Command> trajectoryChooser = new SendableChooser<>();
-  private final SendableChooser<Command> autoChooser = new SendableChooser<>();
+    // Auto Commands
+    private final MiddleStart middleStart = new MiddleStart(driveBase, shooter, intake);
+    private final LeftStart leftStart = new LeftStart(driveBase);
+    private final SendableChooser<Command> trajectoryChooser = new SendableChooser<>();
+    private final SendableChooser<Command> autoChooser = new SendableChooser<>();
 
-  // TeleOp Commands
-  private final NewFieldDrive NFD = new NewFieldDrive(driveBase,
-                                        chassisCtrl::getLeftY,
-                                        chassisCtrl::getLeftX,
-                                        chassisCtrl::getRightX);
-  private final StopEverything stopEverything = new StopEverything(intake, shooter);
-  private final SmartShoot smartShoot = new SmartShoot(shooter, intake);
-  private final NearShoot nearShoot = new NearShoot(shooter, intake);
-  private final FarShoot farShoot = new FarShoot(shooter, intake);
-  private final JustShoot justShoot = new JustShoot(shooter, intake);
-  private final SmartIntake smartIntake = new SmartIntake(shooter, intake);
-  private final BackToOrigin backToOrigin = new BackToOrigin(climber, intake, shooter);
-  private final Amp ampCmd = new Amp(climber, shooter, intake);
-  private final SafeClimbTop safeClimbTop = new SafeClimbTop(climber, shooter);
-  private final ShooterSuck shooterSuck = new ShooterSuck(shooter);
+    // TeleOp Commands
+    private final NewFieldDrive NFD = new NewFieldDrive(driveBase,
+            chassisCtrl::getLeftY,
+            chassisCtrl::getLeftX,
+            chassisCtrl::getRightX);
+    private final StopEverything stopEverything = new StopEverything(intake, shooter);
+    private final SmartShoot smartShoot = new SmartShoot(shooter, intake);
+    private final NearShoot nearShoot = new NearShoot(shooter, intake);
+    private final FarShoot farShoot = new FarShoot(shooter, intake);
+    private final JustShoot justShoot = new JustShoot(shooter, intake);
+    private final SmartIntake smartIntake = new SmartIntake(shooter, intake);
+    private final BackToOrigin backToOrigin = new BackToOrigin(climber, intake, shooter);
+    private final Amp ampCmd = new Amp(climber, shooter, intake);
+    private final SafeClimbTop safeClimbTop = new SafeClimbTop(climber, shooter);
+    private final ShooterSuck shooterSuck = new ShooterSuck(shooter);
 
-  private final static File[] pathFileList = new File(Filesystem.getDeployDirectory(), "pathplanner/paths").listFiles();
+    private final static File[] pathFileList = new File(Filesystem.getDeployDirectory(), "pathplanner/paths").listFiles();
 
-  public RobotContainer() {
-    // Configure controller buttons
-    configureTestingBindings();
-    configureYuJieBindings();
-    createButtonsOnDS();
+    public RobotContainer() {
+        // Configure controller buttons
+        configureTestingBindings();
+        configureYuJieBindings();
+        createButtonsOnDS();
 
-    driveBase.setDefaultCommand(NFD);
-  }
+        driveBase.setDefaultCommand(NFD);
+    }
 
-  private void configureTestingBindings() {
-    new JoystickButton(chassisCtrl, 1).onTrue(new InstantCommand(() -> shooter.setPosition(50), shooter));
-    new JoystickButton(chassisCtrl, 3).onTrue(shooterSuck).onFalse(new InstantCommand(shooter::stopShoot));
+    private void configureTestingBindings() {
+        new JoystickButton(chassisCtrl, 1).onTrue(new InstantCommand(() -> shooter.setPosition(50), shooter));
+        new JoystickButton(chassisCtrl, 3).onTrue(shooterSuck).onFalse(new InstantCommand(shooter::stopShoot));
 
-    new POVButton(chassisCtrl, 0).onTrue(nearShoot);
-    new POVButton(chassisCtrl, 180).onTrue(farShoot);
-    new POVButton(chassisCtrl, 270).whileTrue(justShoot);
+        new POVButton(chassisCtrl, 0).onTrue(nearShoot);
+        new POVButton(chassisCtrl, 180).onTrue(farShoot);
+        new POVButton(chassisCtrl, 270).whileTrue(justShoot);
 //    new POVButton(chassisCtrl, 180).whileTrue(smartShootNear);
 
-    new JoystickButton(chassisCtrl, 5).whileTrue(new InstantCommand(intake::reverseConvey)).onFalse(new InstantCommand(intake::stopAll));
-    new JoystickButton(chassisCtrl, 6).whileTrue(new InstantCommand(intake::shoot)).onFalse(new InstantCommand(intake::stopAll));
+        new JoystickButton(chassisCtrl, 5).whileTrue(new InstantCommand(intake::reverseConvey)).onFalse(new InstantCommand(intake::stopAll));
+        new JoystickButton(chassisCtrl, 6).whileTrue(new InstantCommand(intake::shoot)).onFalse(new InstantCommand(intake::stopAll));
 
-    new JoystickButton(chassisCtrl, 7).onTrue(new InstantCommand(driveBase::setIntakeAsHead));
-  }
+        new JoystickButton(chassisCtrl, 7).onTrue(new InstantCommand(driveBase::setIntakeAsHead));
+    }
 
-  private void configureYuJieBindings() {
-    new JoystickButton(assistCtrl, 1).onTrue(ampCmd);
-    new JoystickButton(assistCtrl, 2).onTrue(backToOrigin);
-    new JoystickButton(assistCtrl, 3).onTrue(smartIntake);
-    new JoystickButton(assistCtrl, 4).whileTrue(new InstantCommand(intake::shoot, intake)).onFalse(new InstantCommand(intake::stopIntake));
+    private void configureYuJieBindings() {
+        new JoystickButton(assistCtrl, 1).onTrue(ampCmd);
+        new JoystickButton(assistCtrl, 2).onTrue(backToOrigin);
+        new JoystickButton(assistCtrl, 3).onTrue(smartIntake);
+        new JoystickButton(assistCtrl, 4).whileTrue(new InstantCommand(intake::shoot, intake)).onFalse(new InstantCommand(intake::stopIntake));
 
-    new JoystickButton(assistCtrl, 5).whileTrue(new InstantCommand(intake::setFloorAngle)
-            .alongWith(new InstantCommand(intake::suck))).onFalse(new InstantCommand(intake::stopAll));
-    new JoystickButton(assistCtrl, 6).whileTrue(new InstantCommand(intake::reverseConvey)).onFalse(new InstantCommand(intake::stopAll));
+        new JoystickButton(assistCtrl, 5).whileTrue(new InstantCommand(intake::setFloorAngle)
+                .alongWith(new InstantCommand(intake::suck))).onFalse(new InstantCommand(intake::stopAll));
+        new JoystickButton(assistCtrl, 6).whileTrue(new InstantCommand(intake::reverseConvey)).onFalse(new InstantCommand(intake::stopAll));
 
-    new POVButton(assistCtrl, 0).whileTrue(new InstantCommand(climber::up, climber)).onFalse(new InstantCommand(climber::stop));
-    new POVButton(assistCtrl, 180).whileTrue(new InstantCommand(climber::down, climber)).onFalse(new InstantCommand(climber::stop));
-    new POVButton(assistCtrl, 270).whileTrue(new InstantCommand(climber::setFloorLevel, climber));
-  }
+        new POVButton(assistCtrl, 0).whileTrue(new InstantCommand(climber::up, climber)).onFalse(new InstantCommand(climber::stop));
+        new POVButton(assistCtrl, 180).whileTrue(new InstantCommand(climber::down, climber)).onFalse(new InstantCommand(climber::stop));
+        new POVButton(assistCtrl, 270).whileTrue(new InstantCommand(climber::setFloorLevel, climber));
+    }
 
-  public void createButtonsOnDS() {
-    // Climber
-    SmartDashboard.putData("Climber Up", new InstantCommand(climber::up, climber));
-    SmartDashboard.putData("Climber Down", new InstantCommand(climber::down, climber));
-    SmartDashboard.putData("Climber Stop", new InstantCommand(climber::stop, climber));
+    public void createButtonsOnDS() {
+        // Climber
+        SmartDashboard.putData("Climber Up", new InstantCommand(climber::up, climber));
+        SmartDashboard.putData("Climber Down", new InstantCommand(climber::down, climber));
+        SmartDashboard.putData("Climber Stop", new InstantCommand(climber::stop, climber));
 
-    // Intake
-    SmartDashboard.putData("Intake Up", new InstantCommand(intake::up, intake));
-    SmartDashboard.putData("Intake Down", new InstantCommand(intake::down, intake));
-    SmartDashboard.putData("Intake Stop", new InstantCommand(intake::stopAngle, intake));
-    SmartDashboard.putData("Intake Suck", new InstantCommand(intake::suck, intake));
-    SmartDashboard.putData("Intake Shoot", new InstantCommand(intake::shoot, intake));
-  }
+        // Intake
+        SmartDashboard.putData("Intake Up", new InstantCommand(intake::up, intake));
+        SmartDashboard.putData("Intake Down", new InstantCommand(intake::down, intake));
+        SmartDashboard.putData("Intake Stop", new InstantCommand(intake::stopAngle, intake));
+        SmartDashboard.putData("Intake Suck", new InstantCommand(intake::suck, intake));
+        SmartDashboard.putData("Intake Shoot", new InstantCommand(intake::shoot, intake));
+    }
 
-  public Command getAutonomousCommand() {
+    public Command getAutonomousCommand() {
 //       return new BlueTest2(driveBase);
 //       return trajectoryChooser.getSelected();
-       return middleStart;
+        return middleStart;
 //    return new SingleTrajectory(driveBase, "simple go out");
-  }
-
-  public void initiateTrajectoryChooser() {
-    // Initiate SendableChooser
-    List<String> trajectoryList = new ArrayList<>();
-    System.out.println("Detected paths:");
-    for (File file: Objects.requireNonNull(pathFileList)) {
-      if (file.isFile()) {
-        String pureFileName = file.getName().replaceFirst("[.][^.]+$", "");  // Use regex to remove extension
-        trajectoryList.add(pureFileName);
-        System.out.println(" " + pureFileName);
-      }
     }
-    trajectoryChooser.setDefaultOption("Run all", middleStart);
-    for (String name: trajectoryList) {
-      trajectoryChooser.addOption(name, new SingleTrajectory(driveBase, name));
+
+    public void initiateTrajectoryChooser() {
+        // Initiate SendableChooser
+        List<String> trajectoryList = new ArrayList<>();
+        System.out.println("Detected paths:");
+        for (File file : Objects.requireNonNull(pathFileList)) {
+            if (file.isFile()) {
+                String pureFileName = file.getName().replaceFirst("[.][^.]+$", "");  // Use regex to remove extension
+                trajectoryList.add(pureFileName);
+                System.out.println(" " + pureFileName);
+            }
+        }
+        trajectoryChooser.setDefaultOption("Run all", middleStart);
+        for (String name : trajectoryList) {
+            trajectoryChooser.addOption(name, new SingleTrajectory(driveBase, name));
+        }
+        SmartDashboard.putData("Choose Trajectory", trajectoryChooser);
     }
-    SmartDashboard.putData("Choose Trajectory", trajectoryChooser);
-  }
 
-  public void initiateAutoChooser(){
-    autoChooser.setDefaultOption("MiddleStart", leftStart);
-    autoChooser.addOption("LeftStart", middleStart);
+    public void initiateAutoChooser() {
+        autoChooser.setDefaultOption("MiddleStart", leftStart);
+        autoChooser.addOption("LeftStart", middleStart);
 
-    SmartDashboard.putData("Choose Auto", autoChooser);
-  }
+        SmartDashboard.putData("Choose Auto", autoChooser);
+    }
 }
