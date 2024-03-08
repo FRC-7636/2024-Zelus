2ced2b227ff3e8c059a3a84e8a0bddda5d2f0884
Allen95Wei <allen95wei@gmail.com>
2024-03-08 16:20:25 +0300

    自動時之射擊指令將會在2秒時強制結束，以避免「NOTE未吸入成功就停在最後路徑」的問題
    Signed-off-by: Allen95Wei <allen95wei@gmail.com>

--- src/main/java/frc/robot/commands/AUTO_CMD/MiddleStart.java
+++ src/main/java/frc/robot/commands/AUTO_CMD/MiddleStart.java
@@ -3,6 +3,7 @@ package frc.robot.commands.AUTO_CMD;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
+import frc.robot.LimelightHelpers;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Swerve;
@@ -14,10 +15,15 @@ public class MiddleStart extends SequentialCommandGroup {
         addCommands(new InstantCommand(intake::setFloorAngle));
         addCommands(new InstantCommand(() -> shooter.setPosition(50)));
         addCommands(new AutoShoot(shooter, intake));
-//        addCommands(Commands.runOnce(() -> m_swerve.resetOdometry(LimelightHelpers.getBotPose2d_wpiBlue("")), m_swerve));
+        addCommands(new InstantCommand(() -> m_swerve.resetOdometry(LimelightHelpers.getBotPose2d_wpiBlue("")), m_swerve));
         addCommands(new SmartIntake(shooter, intake)
-                .alongWith(m_swerve.getAutonomousCommand("blue 2 1", true)));
-        addCommands(m_swerve.getAutonomousCommand("blue 2 2", false));
+                .alongWith(m_swerve.getAutonomousCommand("mid 1", true)));
+        addCommands(new AutoShoot(shooter, intake).withTimeout(2));
+        addCommands(new SmartIntake(shooter, intake)
+                .alongWith(m_swerve.getAutonomousCommand("mid 3 2", false)));
+        addCommands(new AutoShoot(shooter, intake).withTimeout(2));
+        addCommands(new SmartIntake(shooter, intake)
+                .alongWith(m_swerve.getAutonomousCommand("mid 3 3", false)));
         addCommands(new AutoShoot(shooter, intake));
 //        addCommands(new SmartIntake(shooter, intake)
 //                .alongWith(m_swerve.getAutonomousCommand("mid 3", false)));
