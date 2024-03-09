e9a8ffb79bffed4976fbc9f3dc7e21f65bbe3487
Allen95Wei <allen95wei@gmail.com>
2024-03-09 12:22:18 +0300

    更改AutoIntake格式
    Signed-off-by: Allen95Wei <allen95wei@gmail.com>

--- src/main/java/frc/robot/commands/AUTO_CMD/AutoIntake.java
+++ src/main/java/frc/robot/commands/AUTO_CMD/AutoIntake.java
@@ -1,18 +1,35 @@
 package frc.robot.commands.AUTO_CMD;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.Command;
 
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 import frc.robot.Constants.ShooterConstants;
 
-public class AutoIntake extends SequentialCommandGroup {
+public class AutoIntake extends Command {
+    private final Intake intake;
+    private final Shooter shooter;
 
     public AutoIntake(Shooter shooter, Intake intake) {
-        addCommands(new InstantCommand(() -> shooter.setPosition(ShooterConstants.Control.INTAKE_POSITION)));
-        addCommands(new InstantCommand(intake::setFloorAngle));
-        addCommands(new InstantCommand(intake::suck));
-        addCommands(new InstantCommand(intake::startConvey));
+        this.intake = intake;
+        this.shooter = shooter;
+        // each subsystem used by the command must be passed into the
+        // addRequirements() method (which takes a vararg of Subsystem)
+        addRequirements(this.intake, this.shooter);
+    }
+
+
+    @Override
+    public void execute() {
+        shooter.setPosition(ShooterConstants.Control.INTAKE_POSITION);
+        intake.setFloorAngle();
+        intake.suck();
+        intake.startConvey();
+        shooter.transport();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return shooter.noteDetected();
     }
 }
